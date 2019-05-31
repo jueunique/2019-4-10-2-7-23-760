3f5127a989afe36fab10ac4d5cef16e264aa7555
gjx1 <gong0jiaxin@163.com>
2019-05-31 20:10:51 +0800

    add code to main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,4 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    return Math.round(inputs.parkTime * 0.25 + 6 + Math.max(0, (inputs.distance - 2) * 0.8) + Math.max(0, (inputs.distance - 8) * 0.4));
 };
